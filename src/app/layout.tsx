39933dfe1b48ebd658814f9a5c7136bfa355ca45
Codemyhobby <alhassandamilola16@gmail.com>
2024-10-13 19:00:44 +0100

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,8 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-    title: "Donki",
-    description: "theDonki description",
+    title: "theDonki.org - AI-Powered Bible Study & Scriptural Research",
+    description: `Experience theDonki.org, the revolutionary AI-powered platform for Bible study and scriptural research. Search Bible passages, get answers, and explore Greek and Hebrew texts. Powered by technology to enhance your understanding of God’s Word.`,
 };
 
 export default function RootLayout({
